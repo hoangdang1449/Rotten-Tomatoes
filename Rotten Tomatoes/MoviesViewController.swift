4da689b3deb10cc476995aeb4f36cada32ee3d87
hoangdang1449 <hoangdang1449@gmail.com>
2015-08-29 23:16:30 +0700

    Make change with poster
    way to go!

--- Rotten Tomatoes/MoviesViewController.swift
+++ Rotten Tomatoes/MoviesViewController.swift
@@ -14,35 +14,36 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     var movies: [NSDictionary]?
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
+       
         super.viewDidLoad()
         
         let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
 
         let request = NSURLRequest(URL: url)
         
-        NSURLConnection.sendAsynchronousRequest(request,queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
             
         let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? NSDictionary
             
-            if let json = json {
+        if let json = json {
                 self.movies = json["movies"] as? [NSDictionary]
                 self.tableView.reloadData()
             }
         }
         
         tableView.dataSource = self
-        tableView.dataSource = self
+        tableView.delegate = self
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+       }
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    
-    {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         if let movies = movies {
             return movies.count
         } else {
@@ -50,19 +51,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         }
     }
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-    
-    {
-        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! UITableViewCell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let movie = movies! [indexPath.row]
+        var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         
-        cell.textLabel?.text = movie["title"] as? String
+        let movie = movies![indexPath.row]
+        
+        cell.titleLabel.text = movie["title"] as? String
+        cell.synopsisLabel.text = movie["synopsis"] as? String
+        
+        let url = NSURL(string:movie.valueForKeyPath("posters.thumbnail") as! String)!
+        cell.posterView.setImageWithURL(url)
         
         return cell
     }
 
-    /*
+
+    /* 
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
